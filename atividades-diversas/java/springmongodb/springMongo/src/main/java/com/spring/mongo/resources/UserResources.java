2d8a63653aa5d6bb3f3148fa014842ae6b66b6be
Leozin89Python <leozin069@gmail.com>
2021-01-08 01:05:41 -0300

    exercicios-java-mysql-workbench-react-vue-e-outros

--- atividades-diversas/java/springmongodb/springMongo/src/main/java/com/spring/mongo/resources/UserResources.java
+++ atividades-diversas/java/springmongodb/springMongo/src/main/java/com/spring/mongo/resources/UserResources.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.mongo.domain.User;
+import com.spring.mongo.services.UserServices;
 
 @RestController
 @RequestMapping(value="/users")
@@ -24,12 +25,12 @@ public class UserResources 	{
 	 
 	 */
 	
+	@Autowired
+	private UserServices service;  
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User Isabela = new User("1", "Isabela Gatinha", "maria@eu.com");
-		User Rebeca = new User("2", "Rebeca Amor", "becacoracao@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(Isabela,Rebeca));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
