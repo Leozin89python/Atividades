362b4839439a69866a53663b497c50083544d870
Leozin89Python <leozin069@gmail.com>
2021-01-23 03:13:42 -0300

    exercicios-java-mysql-workbench-e-outros

--- atividades-diversas/jpa-java/jpa-hibernate-springboot/src/spring-java/demo/src/main/java/com/server/entities/Order.java
+++ atividades-diversas/jpa-java/jpa-hibernate-springboot/src/spring-java/demo/src/main/java/com/server/entities/Order.java
@@ -101,16 +101,24 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 	
+	public Set<OrderItem> getItems(){
+		return items;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-	}
-
-	public Set<OrderItem> getItems(){
-		return items;
 	}
 	
 	@Override
